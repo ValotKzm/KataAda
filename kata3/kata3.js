22ab449f95daca765dcc377b48804a65aa4eeb94
ValotKzm <darkvalot@gmail.com>
2025-09-26 12:00:12 +0200

    Added kata3

---
+++ kata3/kata3.js
@@ -0,0 +1,53 @@
+const calendrierDuCrayon = {
+  septembre: 10,
+  octobre: 9,
+  novembre: 8,
+  decembre: 7,
+  janvier: 6,
+  fevrier: 5,
+  mars: 4,
+  avril: 3,
+  mai: 2,
+  juin: 1,
+};
+//  /\
+// /__\
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// ||||
+// |__|
+// |  |
+// |__|
+
+function drawMyPen(mois) {
+  const etages = calendrierDuCrayon[mois];
+  if (!etages) {
+    return console.log("Mois invalide");
+  } else {
+    console.log(" /\\ ");
+    console.log("/__\\");
+    for (let i = 0; i < etages; i++) {
+      console.log("||||");
+    }
+  }
+
+  if (etages <= 4) {
+    console.log("|__|");
+    console.log("|__|");
+    return console.log(`voici le crayon pour le mois de ${mois}`);
+  } else {
+    console.log("|__|");
+    console.log("|  |");
+    console.log("|__|");
+    return console.log(`voici le crayon pour le mois de ${mois}`);
+  }
+}
+drawMyPen("testerreur");
+drawMyPen("juin");
